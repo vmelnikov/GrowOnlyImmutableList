49a492e6af0a98d56e14cfc243f27ed451882f93
vadim <vmelnikov@gmail.com>
2022-02-16 16:27:01 +0300

    add GrowOnlyImmutableList.csproj

---
+++ src/GrowOnlyImmutableList/GrowOnlyImmutableList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace Mvo.GrowOnlyImmutableList;
+
+public class GrowOnlyImmutableList<T> : IGrowOnlyImmutableList<T>
+{
+    public IGrowOnlyImmutableList<T> Add(T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public int Count { get; }
+
+    public T this[int index] => throw new NotImplementedException();
+}
