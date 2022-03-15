95cfd85c24bdc0d94c9d428af49bb30e1a06a62e
vadim <vmelnikov@gmail.com>
2022-03-15 17:01:14 +0300

    add documentation

--- src/GrowOnlyImmutableList/GrowOnlyImmutableList.cs
+++ src/GrowOnlyImmutableList/GrowOnlyImmutableList.cs
@@ -2,17 +2,28 @@
 
 namespace Mvo.GrowOnlyImmutableList;
 
+/// <summary>
+/// Represents a strongly typed threadsafe immutable list of objects that can be accessed by index. Provide only add items method
+/// </summary>
+/// <typeparam name="T">The type of elements in the list.</typeparam>
 public class GrowOnlyImmutableList<T> : IGrowOnlyImmutableList<T>
 {
     private const int DefaultCapacity = 4;
     private readonly T[] _items;
     private int _growAttemptsCount;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GrowOnlyImmutableList{T}" /> class that is empty and has the default initial capacity.
+    /// </summary>
     public GrowOnlyImmutableList()
     {
         _items = Array.Empty<T>();
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GrowOnlyImmutableList{T}" /> class that is empty and has the specified initial capacity.
+    /// </summary>
+    /// <param name="capacity">The number of elements that the new list can initially store.</param>
     public GrowOnlyImmutableList(int capacity)
     {
         _items = new T[capacity];
@@ -24,14 +35,32 @@ public class GrowOnlyImmutableList<T> : IGrowOnlyImmutableList<T>
         Count = count;
     }
 
+    /// <inheritdoc cref="IReadOnlyCollection{T}"/>
     public int Count { get; }
 
+    /// <inheritdoc cref="IReadOnlyList{T}"/>
     public T this[int index] =>
         _items[index];
 
+
+    /// <inheritdoc/>
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (var i = 0; i < Count; i++)
+            yield return _items[i];
+    }
+
+    /// <inheritdoc/>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    /// <inheritdoc/>
     public IGrowOnlyImmutableList<T> Add(T value)
     {
         var growAttemptsCount = Interlocked.Increment(ref _growAttemptsCount);
+        //We must create a copy of items array if Add method called twice or more for one instance of list
         var items = Count == _items.Length ? GrowItems() : GetItems(growAttemptsCount > 1);
         items[Count] = value;
         return new GrowOnlyImmutableList<T>(items, Count + 1);
@@ -47,7 +76,7 @@ public class GrowOnlyImmutableList<T> : IGrowOnlyImmutableList<T>
     }
 
     /// <summary>
-    /// Grow array twice and copy items into it
+    /// Increases capacity of items array to twice the current capacity and copy items into it
     /// </summary>
     /// <returns>Array with new capacity</returns>
     private T[] GrowItems()
@@ -59,16 +88,5 @@ public class GrowOnlyImmutableList<T> : IGrowOnlyImmutableList<T>
         var newItems = new T[newCapacity];
         Array.Copy(_items, newItems, Count);
         return newItems;
-    }
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        for (var i = 0; i < Count; i++)
-            yield return _items[i];
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
     }
 }
--- src/GrowOnlyImmutableList/IGrowOnlyImmutableList.cs
+++ src/GrowOnlyImmutableList/IGrowOnlyImmutableList.cs
@@ -2,5 +2,10 @@ namespace Mvo.GrowOnlyImmutableList;
 
 public interface IGrowOnlyImmutableList<T> : IReadOnlyList<T>
 {
+    /// <summary>
+    /// Adds given object to the end of the list
+    /// </summary>
+    /// <param name="value">The object to be added</param>
+    /// <returns>New list with added object</returns>
     IGrowOnlyImmutableList<T> Add(T value);
 }
