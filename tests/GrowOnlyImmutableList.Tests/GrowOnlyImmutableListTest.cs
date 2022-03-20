0c51a78a2b4d6e1cfad3102cf1c448641fc8e1bb
vadim <vmelnikov@gmail.com>
2022-03-20 14:49:08 +0300

    add AddRange methods benchmark

--- tests/GrowOnlyImmutableList.Tests/GrowOnlyImmutableListTest.cs
+++ tests/GrowOnlyImmutableList.Tests/GrowOnlyImmutableListTest.cs
@@ -14,4 +14,15 @@ public class GrowOnlyImmutableListTest
             list = list.Add(i);
         Assert.Equal(count, list.Count);
     }
+    
+    [Fact]
+    public void AddRangeTest()
+    {
+        var itemsForAdd = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        const int addCount = 1000;
+        var list = new GrowOnlyImmutableList<int>();
+        for (var i = 0; i < addCount; i++)
+            list = list.AddRange(itemsForAdd);
+        Assert.Equal(addCount * itemsForAdd.Length, list.Count);
+    }
 }
